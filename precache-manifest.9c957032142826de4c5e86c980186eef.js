72e8c146cc621fb4615bcccff2da5266cb0fb39d
tamara-f <filatovatam@ukr.net>
2020-08-27 21:58:58 +0300

    Updates

--- precache-manifest.9c957032142826de4c5e86c980186eef.js
+++ precache-manifest.9c957032142826de4c5e86c980186eef.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2410a04518f87c9f57b0b8b0152f97b9",
+    "revision": "4c680ec979820e7a4e4c5495b3c5137c",
     "url": "/goit-react-hw-01-components/index.html"
   },
   {
-    "revision": "897c87ff8bf41eb8dd1e",
-    "url": "/goit-react-hw-01-components/static/css/main.87804ee3.chunk.css"
+    "revision": "534b2a4ec0c2166bb175",
+    "url": "/goit-react-hw-01-components/static/css/main.04c17c6b.chunk.css"
   },
   {
     "revision": "8702e87f2de9d9d43916",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/goit-react-hw-01-components/static/js/2.f39d56b2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "897c87ff8bf41eb8dd1e",
-    "url": "/goit-react-hw-01-components/static/js/main.5b82acb4.chunk.js"
+    "revision": "534b2a4ec0c2166bb175",
+    "url": "/goit-react-hw-01-components/static/js/main.2b0b74ca.chunk.js"
   },
   {
     "revision": "7c1abe1df921e466788f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-01-components/precache-manifest.17fc10c85827dbb08cdbf4a3a3fe14a1.js"
+  "/goit-react-hw-01-components/precache-manifest.9c957032142826de4c5e86c980186eef.js"
 );
 
 self.addEventListener('message', (event) => {
